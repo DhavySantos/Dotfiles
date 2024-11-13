8c594d7d894e8acc955d1031d3125c4557cf6bf8
Dhavy Santos <dhavy.s@proton.me>
2024-11-12 21:17:47 -0300

    feat: also check if ffmpeg are running

--- .scripts/amdgpu.sh
+++ .scripts/amdgpu.sh
@@ -5,22 +5,22 @@ last_state="low"
 
 # Infinite loop with a sleep interval of 5 seconds
 while true; do
-  # Check for the presence of a gamescope window
-  if hyprctl clients | grep "class: gamescope" > /dev/null; then
-    # If gamescope is detected, check if the last state is not high
+  # Check for the presence of a gamescope window or an ffmpeg process
+  if hyprctl clients | grep "class: gamescope" > /dev/null || pgrep -fl ffmpeg | grep "ffmpeg" > /dev/null; then
+    # If either gamescope or ffmpeg is detected, check if the last state is not high
     if [ "$last_state" != "high" ]; then
-      echo "Gamescope detected. Changing performance level to high."
+      echo "Gamescope or ffmpeg detected. Changing performance level to high."
       sudo /opt/rocm/bin/rocm-smi --setperflevel high
       last_state="high"  # Update the last state to high
     fi
-  else
-    # If gamescope is not detected, check if the last state is not low
+  else # If neither gamescope nor ffmpeg is detected, check if the last state is not low
     if [ "$last_state" != "low" ]; then
-      echo "No gamescope detected. Changing performance level to low."
+      echo "No gamescope or ffmpeg detected. Changing performance level to low."
       sudo /opt/rocm/bin/rocm-smi --setperflevel low
       last_state="low"  # Update the last state to low
     fi
   fi
+
   # Wait for 5 seconds before checking again
   sleep 5
 done
