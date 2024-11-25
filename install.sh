6fcd5ef5c408557cef07b25785874a4e251f2699
Dhavy Santos <dhavy.s@proton.me>
2024-11-25 00:51:49 -0300

    feat: add yay installation / remove tmux plugin manager

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo pacman -Sy --noconfirm \
 	xorg-xrandr neovim stow \
 	kitty zsh starship zoxide \
 	exa tmux feh picom rustup \
-	libcanberra
+	libcanberra yazi hyprpaper
 
 # AMDGPU PACKAGES
 sudo pacman -Sy --noconfirm \
@@ -16,15 +16,20 @@ sudo pacman -Sy --noconfirm \
 git clone https://aur.archlinux.org/yay.git /tmp/yay
 (cd /tmp/yay && makepkg -si --noconfirm)
 
-mkdir -p $HOME/.config/{hypr,i3,rofi,tmux/plugins,waybar,kitty}
+# YAY PACKAGES
+yay -S --noconfirm \
+  apple_cursor gruvbox-material-gtk-theme
+  xremap-wlroots-bin hyprshoot
+
+mkdir -p $HOME/.config/{hypr,i3,rofi,tmux/plugins,waybar,kitty} $HOME/Pictures $HOME/.scripts
+curl -o $HOME/Pictures/wallpaper.jpg https://i.imgur.com/4lqHk7C.jpeg
 
 # CREATE .DOTFILES LINKS
 stow --ignore=".(sh)$" .
 
 # CLONE PLUGINS
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/plugins/syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/autosuggestions
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/pluings/tpm
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/plugins/syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/plugins/autosuggestions
 
 # SETUP RUST
 rustup default stable
